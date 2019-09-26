40ce71d423d371315a26979616e584434c069eac
BioProfe <53934668+BioProfe@users.noreply.github.com>
2019-09-26 07:41:48 +0200

    Create AssemblyInfo.cs

---
+++ WorkflowActivitiesViewer/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WorkflowActivitiesViewer")]
+[assembly: AssemblyDescription("Workflow Activities Viewer is used to see the code activities dependencies visually. That is, when you select a code activity, you will see what workflows execute this code activity, and when you select a workflow, you will which Code Activities are executed by this workflow")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("BioProfe")]
+[assembly: AssemblyProduct("WorkflowActivitiesViewer")]
+[assembly: AssemblyCopyright("Copyright © BioProfe 2019")]
+[assembly: AssemblyTrademark("BioProfe")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("50015ca7-8e0d-4299-91e8-25fbc90de44d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.2019.9.2")]
+[assembly: AssemblyFileVersion("1.2019.9.2")]
